ea6a5e11ed46a6961dd5b2d301079db28079f0d0
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-04-02 17:46:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-let interval = setInterval(() => {
+
+    let interval = setInterval(() => {
     location.href = 'https://fr.linkedin.com/in/dragan-manojlovic-76985310b?trk=people-guest_people_search-card'
     }, 5000)
+  
