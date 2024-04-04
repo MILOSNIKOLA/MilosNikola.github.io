efb1b68df6fd94105f9a79c65e7b9aef6ec861ed
Dragan Manojlovic <milosnikola@outlook.fr>
2024-04-04 18:24:17 +0200

    code4

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
+
 
     let interval = setInterval(() => {
     location.href = 'https://fr.linkedin.com/in/dragan-manojlovic-76985310b?trk=people-guest_people_search-card'
     }, 5000)
-=======
-
-    let interval = setInterval(() => {
-    location.href = 'https://fr.linkedin.com/in/dragan-manojlovic-76985310b?trk=people-guest_people_search-card'
-    }, 5000)
->>>>>>> a6c42f03f4db2dc00b2e5db740a78234e5286c4a
-  
