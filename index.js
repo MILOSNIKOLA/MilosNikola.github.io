af842a0f28ad57c117edf4b26102198469412152
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-08-10 15:31:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -55,61 +55,7 @@ setInterval(() => {
     dotM.style.transform = `rotate(${m * 6}deg)`;
     dotS.style.transform = `rotate(${s * 6}deg)`;
 
-    if (media500) {
-        setInterval1(() => {
-            // get time indicator elements
-            let hours = document.getElementById('hours');
-            let minutes = document.getElementById('minutes');
-            let secondes = document.getElementById('seconds');
-            // let ampm = document.getElementById('ampm');
-        
-            // digits time indicator
-            let hh = document.getElementById('hh');
-            let mm = document.getElementById('mm');
-            let ss = document.getElementById('ss');
-        
-        
-            // dot time indicator
-            let dotH = document.querySelector('.h_dot');
-            let dotM = document.querySelector('.m_dot');
-            let dotS = document.querySelector('.s_dot');
-        
-            // get current time
-            let h = new Date().getHours();
-            let m = new Date().getMinutes();
-            let s = new Date().getSeconds();
-            // let ap = h >= 24 ? '' : '';
-        
-            // convert to 12 hour format
-            if (h > 24) {
-                h = h - 12;
-            }
-        
-            // add 0 before single digit
-            h = h < 10 ? '0' + h : h;
-            m = m < 10 ? '0' + m : m;
-            s = s < 10 ? '0' + s : s;
-        
-            // set time and label
-            hours.innerHTML = h + 'Hours';
-            minutes.innerHTML = m + 'Minutes';
-            secondes.innerHTML = s + 'Seconds';
-            // ampm.innerHTML = ap;
-        
-            // set time circular indicator
-            hh.style.strokeDashoffset = 310 - (310 * h) / 12;
-            mm.style.strokeDashoffset = 310 - (310 * m) / 60;
-            ss.style.strokeDashoffset = 310 - (310 * s) / 60;
-        
-            // set dot time position indicator
-            dotH.style.transform = `rotate(${h * 30}deg)`;
-            dotM.style.transform = `rotate(${m * 6}deg)`;
-            dotS.style.transform = `rotate(${s * 6}deg)`;
-        
-        }, 1000);
-        
-    }
-
+    
 }, 1000);
 
 const reci = document.querySelector('.p');
@@ -126,11 +72,11 @@ reci.addEventListener('click', () => {
     window.open('https://fr.linkedin.com/in/dragan-manojlovic-76985310b?trk=people-guest_people_search-card')
 });
 
-let setInterval1;
+
 let media500 = window.matchMedia('(max-width: 500px)');
 
-if (media500) {
-    setInterval1(() => {
+if (media500.matches) {
+    setInterval(() => {
         // get time indicator elements
         let hours = document.getElementById('hours');
         let minutes = document.getElementById('minutes');
@@ -171,9 +117,9 @@ if (media500) {
         // ampm.innerHTML = ap;
     
         // set time circular indicator
-        hh.style.strokeDashoffset = 310 - (310 * h) / 12;
-        mm.style.strokeDashoffset = 310 - (310 * m) / 60;
-        ss.style.strokeDashoffset = 190 - (190 * s) / 60;
+        hh.style.strokeDashoffset = 400 - (380 * h) / 12;
+        mm.style.strokeDashoffset = 400 - (310 * m) / 60;
+        ss.style.strokeDashoffset = 400 - (310 * s) / 60;
     
         // set dot time position indicator
         dotH.style.transform = `rotate(${h * 30}deg)`;
