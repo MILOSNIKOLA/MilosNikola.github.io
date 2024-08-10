90e56c01b2a4c10fd3a00b0d1e946102d373c52a
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-08-10 21:14:00 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -55,25 +55,7 @@ setInterval(() => {
     dotM.style.transform = `rotate(${m * 6}deg)`;
     dotS.style.transform = `rotate(${s * 6}deg)`;
 
-    
-}, 1000);
-
-const reci = document.querySelector('.p');
-
-// Ajout d'un gestionnaire d'événements pour 'mouseenter'
-
-reci.addEventListener('click', () => {
-    // Ajout de la classe CSS pour l'effet "hover"
-    reci.classList.toggle('reci');
-    console.log('aaaaaaaaa');
-    // let interval = setInterval(() => {
-    //     location.href = 'https://fr.linkedin.com/in/dragan-manojlovic-76985310b?trk=people-guest_people_search-card' 
-    // }, 1000)
-    window.open('https://fr.linkedin.com/in/dragan-manojlovic-76985310b?trk=people-guest_people_search-card')
-});
-
-
-let media500 = window.matchMedia('(max-width: 500px)');
+    let media500 = window.matchMedia('(max-width: 500px)');
 
 if (media500.matches) {
     setInterval(() => {
@@ -117,7 +99,7 @@ if (media500.matches) {
         // ampm.innerHTML = ap;
     
         // set time circular indicator
-        hh.style.strokeDashoffset = 400 - (380 * h) / 12;
+        hh.style.strokeDashoffset = 400 - (370 * h) / 12;
         mm.style.strokeDashoffset = 400 - (310 * m) / 60;
         ss.style.strokeDashoffset = 400 - (310 * s) / 60;
     
@@ -130,63 +112,81 @@ if (media500.matches) {
     
 }
 
-// let setInterval2;
-// let media460 = window.matchMedia('(max-width: 460px)');
 
-// if (media460) {
-//     setInterval2(() => {
-//         // get time indicator elements
-//         let hours = document.getElementById('hours');
-//         let minutes = document.getElementById('minutes');
-//         let secondes = document.getElementById('seconds');
-//         // let ampm = document.getElementById('ampm');
+}, 1000);
+
+const reci = document.querySelector('.p');
+
+// Ajout d'un gestionnaire d'événements pour 'mouseenter'
+
+reci.addEventListener('click', () => {
+    // Ajout de la classe CSS pour l'effet "hover"
+    reci.classList.toggle('reci');
+    console.log('aaaaaaaaa');
+    // let interval = setInterval(() => {
+    //     location.href = 'https://fr.linkedin.com/in/dragan-manojlovic-76985310b?trk=people-guest_people_search-card' 
+    // }, 1000)
+    window.open('https://fr.linkedin.com/in/dragan-manojlovic-76985310b?trk=people-guest_people_search-card')
+});
+
+
+
+let media460 = window.matchMedia('(max-width: 460px)');
+
+if (media460.matches) {
+    setInterval(() => {
+        // get time indicator elements
+        let hours = document.getElementById('hours');
+        let minutes = document.getElementById('minutes');
+        let secondes = document.getElementById('seconds');
+        // let ampm = document.getElementById('ampm');
     
-//         // digits time indicator
-//         let hh = document.getElementById('hh');
-//         let mm = document.getElementById('mm');
-//         let ss = document.getElementById('ss');
+        // digits time indicator
+        let hh = document.getElementById('hh');
+        let mm = document.getElementById('mm');
+        let ss = document.getElementById('ss');
     
     
-//         // dot time indicator
-//         let dotH = document.querySelector('.h_dot');
-//         let dotM = document.querySelector('.m_dot');
-//         let dotS = document.querySelector('.s_dot');
+        // dot time indicator
+        let dotH = document.querySelector('.h_dot');
+        let dotM = document.querySelector('.m_dot');
+        let dotS = document.querySelector('.s_dot');
     
-//         // get current time
-//         let h = new Date().getHours();
-//         let m = new Date().getMinutes();
-//         let s = new Date().getSeconds();
-//         // let ap = h >= 24 ? '' : '';
+        // get current time
+        let h = new Date().getHours();
+        let m = new Date().getMinutes();
+        let s = new Date().getSeconds();
+        // let ap = h >= 24 ? '' : '';
     
-//         // convert to 12 hour format
-//         if (h > 24) {
-//             h = h - 12;
-//         }
+        // convert to 12 hour format
+        if (h > 24) {
+            h = h - 12;
+        }
     
-//         // add 0 before single digit
-//         h = h < 10 ? '0' + h : h;
-//         m = m < 10 ? '0' + m : m;
-//         s = s < 10 ? '0' + s : s;
+        // add 0 before single digit
+        h = h < 10 ? '0' + h : h;
+        m = m < 10 ? '0' + m : m;
+        s = s < 10 ? '0' + s : s;
     
-//         // set time and label
-//         hours.innerHTML = h + 'Hours';
-//         minutes.innerHTML = m + 'Minutes';
-//         secondes.innerHTML = s + 'Seconds';
-//         // ampm.innerHTML = ap;
+        // set time and label
+        hours.innerHTML = h + 'Hours';
+        minutes.innerHTML = m + 'Minutes';
+        secondes.innerHTML = s + 'Seconds';
+        // ampm.innerHTML = ap;
     
-//         // set time circular indicator
-//         hh.style.strokeDashoffset = 310 - (310 * h) / 12;
-//         mm.style.strokeDashoffset = 310 - (310 * m) / 60;
-//         ss.style.strokeDashoffset = 310 - (310 * s) / 60;
+        // set time circular indicator
+        hh.style.strokeDashoffset = 440 - (80 * h) / 12;
+        mm.style.strokeDashoffset = 440 - (310 * m) / 60;
+        ss.style.strokeDashoffset = 440 - (310 * s) / 60;
     
-//         // set dot time position indicator
-//         dotH.style.transform = `rotate(${h * 30}deg)`;
-//         dotM.style.transform = `rotate(${m * 6}deg)`;
-//         dotS.style.transform = `rotate(${s * 6}deg)`;
+        // set dot time position indicator
+        dotH.style.transform = `rotate(${h * 30}deg)`;
+        dotM.style.transform = `rotate(${m * 6}deg)`;
+        dotS.style.transform = `rotate(${s * 6}deg)`;
     
-//     }, 1000);
+    }, 1000);
     
-// }
+}
 
 
 
