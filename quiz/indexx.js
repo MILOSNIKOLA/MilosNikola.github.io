87fc6393974c775bc1169f8db4cdb9ffa6b4a1a8
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-09-12 17:19:01 +0200

    Add files via upload

--- quiz/indexx.js
+++ quiz/indexx.js
@@ -4,7 +4,7 @@ const btn2 = document.getElementById('btn-2');
 const btn3 = document.getElementById('btn-3');
 const btn4 = document.getElementById('btn-4');
 const response = document.querySelector('p');
-const hidden = document.querySelector('boxx')
+const hidden = document.querySelector('.boxx')
 // const boxx = document.getSelection('box')
 
 // Nombre initial d'essais
@@ -83,4 +83,4 @@ questionContainer.addEventListener('click', () => {
 hidden.addEventListener('click', () => {
     console.log(hidden);
     
-})
+});
